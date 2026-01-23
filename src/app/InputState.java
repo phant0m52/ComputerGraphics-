6f769c1a4a41a8b955517a7ddbe26fcf852425f0
olenenokgg <divanenkogm@gmail.com>
2026-01-23 21:23:53 +0300

    Delete src/app directory

--- src/app/InputState.java
+++
@@ -1,8 +0,0 @@
-package engine;
-
-public interface InputState {
-    boolean isKeyDown(int keyCode);
-    double consumeMouseDeltaX();
-    double consumeMouseDeltaY();
-    default double consumeMouseWheelDelta() { return 0.0; }
-}
